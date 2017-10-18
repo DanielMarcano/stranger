1029ee6b9fe845f5fde49550b4da73ace69f57cf
DanielMarcano <danielemarcano96@gmail.com>
2017-10-18 02:08:59 +0200

    Project reestructured

--- app/email.php
+++ app/email.php
@@ -13,7 +13,7 @@ $message = wordwrap($message, 70, "\n");
 if (preg_match("/[\r\n]/", $email) || $email == '' || $message == '') {
   $response['message'] = 'There was an error trying to send your message...';
 } else {
-  mail("danielmarcanodev@gmail.com", "Querido Cristóbal, tienes un nuevo mensaje de tu página web", $message, $email);
+  mail("danielmarcanodev@gmail.com", "Querido usuario, tienes un nuevo mensaje de tu página web", $message, $email);
   $response['message'] = 'Message sent! Thank you.';
 }
 
